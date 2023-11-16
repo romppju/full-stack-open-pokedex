42f34c531b1047bec166bae435c0642dee5c30a1
Juho Romppainen <juho.romppainen@tuni.fi>
2023-11-16 12:05:12 +0200

    second cypress test added

--- cypress/e2e/pokedex.cy.js
+++ cypress/e2e/pokedex.cy.js
@@ -6,4 +6,10 @@ describe('Pokedex', function () {
       'Pokémon and Pokémon character names are trademarks of Nintendo.'
     )
   })
+
+  it('pokemon can be opened and correct data is shown', function () {
+    cy.visit('http://localhost:5000')
+    cy.contains('ivysaur').click()
+    cy.contains('chlorophyll')
+  })
 })
